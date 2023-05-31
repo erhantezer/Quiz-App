7375fb2a51fd76ceb93b5d7b47178f701b8618ac
Erhan <erhantezer68@gmail.com>
2023-05-31 22:15:10 +0300

    updated project

---
+++ src/context.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+const AppContext = createContext();
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export const AppProvider = ({children}) => {
+
+    
+    return (
+            <AppContext>
+                {children}
+            </AppContext>
+        )
+}
