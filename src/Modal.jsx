c0357aa5cb203f472051a0e0eb2e0a2e2458dc89
Erhan <erhantezer68@gmail.com>
2023-06-03 14:20:07 +0300

    modal m4

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -2,7 +2,18 @@
 
 const Modal = () => {
     return (
-        <div>Modal</div>
+        <div>
+            <div className='modal-content'>
+                <h2>congrats!</h2>
+                <p>
+                    You answered % of
+                    questions correctly
+                </p>
+                <button className='close-btn'>
+                    play again
+                </button>
+            </div>
+        </div>
     )
 }
 
