e71d239e5e871f42b8539013c90dfc90962a375c
Erhan <erhantezer68@gmail.com>
2023-06-03 14:16:59 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,18 @@ function App() {
 
   return (
     <main>
+      <section className="quiz">
+        <p className="correct-answers">correct answers</p>
+        <article className="container">
+        <h2/>
+        <div className="btn-container">
 
+        </div>
+        </article>
+        <button className="next-question">
+          next question
+        </button>
+      </section>
     </main>
   )
 }
