6c89cde45c341c2ae3ea190fdc66801519e81ebe
Erhan <erhantezer68@gmail.com>
2023-06-04 23:30:33 +0300

    handleSubmit added

--- src/SetupForm.jsx
+++ src/SetupForm.jsx
@@ -2,36 +2,36 @@ import { useGlobalContext } from "./context"
 
 
 const SetupForm = () => {
-    const {error, handleSubmit, handleChange, quiz} = useGlobalContext();
-    
+    const { error, handleSubmit, handleChange, quiz } = useGlobalContext();
+
     return (
         <main>
             <section className="quiz quiz-small">
-                <form className="setup-form" onSubmit={handleSubmit}>
+                <form className="setup-form" >
                     <h2>setup quiz</h2>
                     {/* amount */}
                     <div className="form-control">
                         <label htmlFor="amount">number of questions</label>
-                        <input 
-                        type="number"
-                        name="amount"
-                        id="amount"
-                        value={quiz.amount}
-                        className="form-input"
-                        min={1}
-                        max={50}
-                        onChange={handleChange}
+                        <input
+                            type="number"
+                            name="amount"
+                            id="amount"
+                            value={quiz.amount}
+                            className="form-input"
+                            min={1}
+                            max={50}
+                            onChange={handleChange}
                         />
                     </div>
                     {/* category */}
                     <div className="form-control">
                         <label htmlFor="category">category</label>
-                        <select 
-                        name="category" 
-                        id="category"
-                        className="form-input"
-                        value={quiz.category}
-                        onChange={handleChange}
+                        <select
+                            name="category"
+                            id="category"
+                            className="form-input"
+                            value={quiz.category}
+                            onChange={handleChange}
                         >
                             <option value="sports">sports</option>
                             <option value="history">history</option>
@@ -54,13 +54,13 @@ const SetupForm = () => {
                         </select>
                     </div>
                     {error && (
-                            <p className="error">
-                                can't generate questions, please try diffirent options
-                            </p>
-                        )}
-                        <button onClick={null} type="submit" className="submit-btn">
-                            start
-                        </button>
+                        <p className="error">
+                            can't generate questions, please try diffirent options
+                        </p>
+                    )}
+                    <button onClick={handleSubmit} type="submit" className="submit-btn">
+                        start
+                    </button>
                 </form>
             </section>
         </main>
