b21056a413ba93e4b53b931307173d9e09fd125f
Erhan <erhantezer68@gmail.com>
2023-06-11 23:15:52 +0300

    modal edited

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -5,14 +5,14 @@ const Modal = () => {
     const { isModalOpen, closeModal, correct, questions } = useGlobalContext()
     
     return (
-        <div>
+        <div className={`${isModalOpen ? 'modal-container isOpen' : 'modal-container'}`}>
             <div className='modal-content'>
                 <h2>congrats!</h2>
                 <p>
-                    You answered % of
+                    You answered {((correct / questions.length) * 100).toFixed(0)} % of
                     questions correctly
                 </p>
-                <button className='close-btn'>
+                <button className='close-btn' onClick={closeModal}>
                     play again
                 </button>
             </div>
