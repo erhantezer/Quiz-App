3bd57c2ef9f6bb9e94988488314782759347ddb9
Erhan <erhantezer68@gmail.com>
2023-11-08 17:21:42 +0300

    updated

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,8 +1,7 @@
 import { useGlobalContext } from "./context"
 
 const Modal = () => {
-    const { isModalOpen, closeModal, correct, questions } = useGlobalContext()
-
+    const { isModalOpen, closeModal, correct, questions } = useGlobalContext();
     return (
         <div className={`${isModalOpen ? 'modal-container isOpen' : 'modal-container'}`}>
             <div className='modal-content'>
